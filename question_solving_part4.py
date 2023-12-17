640d8f449383b5b5c4c13278e3dd66fe674ec132
Maninder Singh <manindersingh120996@gmail.com>
2023-12-17 20:26:33 +0530

    unique_element code

--- question_solving_part4.py
+++ question_solving_part4.py
@@ -17,5 +17,14 @@ def swap_alternate_elements(arr):
 # swap_alternate_elements([1,2,3,4,5,6,7])
     
 def unique_element(arr):
-    
+    """
+    input = array of size 2m + 1
+    output = 1 unique element
+    e
+    """
+    ans = 0
+    for element in arr:
+        ans = ans^element
+    print("Unique element in array is : ",ans)
+unique_element([11,15,22,15,22,11,3])
 
