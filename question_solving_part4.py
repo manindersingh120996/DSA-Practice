7433f743ec48002e3e67b4954c8e230fbcad9e07
Maninder Singh <manindersingh120996@gmail.com>
2023-12-17 20:24:18 +0530

    update

--- question_solving_part4.py
+++ question_solving_part4.py
@@ -1,4 +1,8 @@
 def swap_alternate_elements(arr):
+    """
+    input = [1,2,3,4,5,6,7]
+    output =  [2, 1, 4, 3, 6, 5, 7]
+    """
     n = len(arr)
     pos = 1
     while pos < n:
@@ -10,5 +14,8 @@ def swap_alternate_elements(arr):
         pos+=2
     print(arr)
 
-swap_alternate_elements([1,2,3,4,5,6,7])
+# swap_alternate_elements([1,2,3,4,5,6,7])
+    
+def unique_element(arr):
+    
 
