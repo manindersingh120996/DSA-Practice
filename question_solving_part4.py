bd9c091d4883dae1056688a71f3b459bfcfec997
Maninder Singh <manindersingh120996@gmail.com>
2023-12-17 21:44:39 +0530

    uniqueOccurance

--- question_solving_part4.py
+++ question_solving_part4.py
@@ -27,5 +27,23 @@ def unique_element(arr):
     for element in arr:
         ans = ans^element
     print("Unique element in array is : ",ans)
-unique_element([11,15,22,15,22,11,3])
+# unique_element([11,15,22,15,22,11,3])
 
+def uniqueOccurance(arr):
+    """
+    Input: arr = [1,2,2,1,1,3]
+    Output: true
+    Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+    """
+    count = {}
+    for num in arr:
+        if num not in count.keys():
+            count[num] = 1
+        elif num in count.keys():
+            count[num] += 1
+    if len(set(count.values())) == len(count):
+        print(True)
+    else:
+        print(False)
+
+uniqueOccurance([1,2,1])
